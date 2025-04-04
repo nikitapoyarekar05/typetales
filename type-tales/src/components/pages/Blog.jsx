a3af8221e62a6315f1e328986b91eace40a32ab4
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-04-04 16:09:38 -0500

    Create Blogposts display

---
+++ type-tales/src/components/pages/Blog.jsx
@@ -0,0 +1,7 @@
+import BlogComponent from "../organism/BlogComponent/index";
+
+const Blog = () => {
+  return <BlogComponent />;
+};
+
+export default Blog;
---
+++ type-tales/src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import BlogPostList from "../organism/BlogPostList";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Blog Posts</h1>
+      <BlogPostList />
+    </>
+  );
+};
+
+export default Home;
