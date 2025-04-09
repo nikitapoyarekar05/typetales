7079d50d803a7389f887e67c949b5b9693ca3d88
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-04-08 22:39:14 -0500

    Add blog post button UI and CSS.
    Also, refactor the code for the BlogPostList component

--- src/components/organism/BlogPostList/index.jsx
+++ src/components/organism/BlogPostList/index.jsx
@@ -4,17 +4,22 @@ import BlogPostItem from "../../molecules/BlogPostItem";
 import BLOG_POSTS from "../../../constants/mockData";
 
 const BlogPostList = () => {
-  if (BLOG_POSTS && BLOG_POSTS.length > 0) {
-    return (
-      <div className={styles.blogPostList}>
-        {BLOG_POSTS.map((post) => (
-          <BlogPostItem key={post.id} {...post} />
-        ))}
+  return (
+    <>
+      <div className={styles.addBlog}>
+        <button className={styles.addBlogBtn}>Add Blog</button>
       </div>
-    );
-  } else {
-    return <p>No blog posts available.</p>;
-  }
+      {BLOG_POSTS.length === 0 ? (
+        <p>No blog posts available.</p>
+      ) : (
+        <div className={styles.blogPostList}>
+          {BLOG_POSTS.map((post) => (
+            <BlogPostItem key={post.id} {...post} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default BlogPostList;
